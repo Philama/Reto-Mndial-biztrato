9c3687ebbbe26bbadf838b1f0d0c830a31813950
vrilli <vrillyjcr@gmail.com>
2022-11-17 20:07:36 -0500

    firebase

---
+++ src/firebase/firebaseconfig.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDtMMYZl6kKP-V8TnFcEvj5kZWR75DmQhw",
+  authDomain: "biztrato-31e11.firebaseapp.com",
+  projectId: "biztrato-31e11",
+  storageBucket: "biztrato-31e11.appspot.com",
+  messagingSenderId: "831141198512",
+  appId: "1:831141198512:web:2326a8f7a907af44525975",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export { app };
