caea560b954dfd936b5f9955663ea222fbfc2cb0
vrilli <vrillyjcr@gmail.com>
2022-11-17 22:41:03 -0500

    listo para merget

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,21 +1,12 @@
-import { Button } from '@chakra-ui/react'
-import React, { useContext } from 'react'
-import { useNavigate } from 'react-router-dom'
-import { logout } from '../actions/AuthAction'
-import { AuthContext } from '../context/authContext'
+
+import React from 'react'
+
 
 const Home = () => {
-  const { dispatch } = useContext(AuthContext)
-  const navigate = useNavigate()
-  const handleLogout = () => {
-      dispatch(logout())
-      navigate('/login')
-  }
+  
 
   return (
-    <div>Home
-      <Button variant="outline" type="submit" fontSize="20px" marginLeft="20px" onClick={handleLogout}>Logout</Button>
-    </div>
+   <div>Hola</div>
   )
 }
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import { Button } from "@chakra-ui/react";
+import React, { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { logout } from "../actions/AuthAction";
+import { AuthContext } from "../context/authContext";
 
-const NavBar = () => {
+const Navbar = () => {
+  const { dispatch } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    dispatch(logout());
+    navigate("/login");
+  };
+
   return (
-    <div>NavBar</div>
-  )
-}
-
-export default NavBar
+    <header>
+      <div>
+        <Button
+          variant="outline"
+          type="submit"
+          fontSize="20px"
+          marginLeft="20px"
+          onClick={handleLogout}
+        >
+          Logout
+        </Button>
+      </div>
+    </header>
+  );
+};
+export default Navbar;
