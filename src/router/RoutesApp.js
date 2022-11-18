89908edf0467630548f57f75acc6a70b49219396
Diego-Fdez <dieghoxcr7@gmail.com>
2022-11-18 07:45:25 -0600

    first commit

--- src/router/RoutesApp.js
+++ src/router/RoutesApp.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from '../components/Home';
-import { Registro } from '../components/Register';
+import { Registro } from '../pages/Register/Register';
 import ActividadScreen from '../pages/Actividad/actividadScreen';
+import Estadisticas from '../pages/Estadisticas/estadisticas';
 
 const RoutesApp = () => {
   return (
     <>
       <Routes>
         <Route path='/' index element={<Home />} />
-        <Route path='/home' element={<Home />} />
         <Route path='/Register' element={<Registro />} />
-        <Route path='/actividades' element={<ActividadScreen />} />
+        <Route path='actividades' element={<ActividadScreen />} />
+        <Route path='estadisticas' element={<Estadisticas />} />
       </Routes>
     </>
   );
